369871eda594d2b6f548ef9da57296f47c4054f3
charlieroth <mail@charlieroth.me>
2025-07-19 23:44:34 +0200

    Echo challenge complete

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,60 +1,103 @@
-use maelstrom::{EchoMsg, EchoOk, EchoOkBody, InitMsg, InitOk, InitOkBody, Node};
-use serde_json::Result;
+use tokio::io::{AsyncBufReadExt, BufReader, stdin};
 
-fn main() -> Result<()> {
-    let init_msg_str = r#"
-    {
-        "src": "c1",
-        "dest": "n3",
-        "body": {
-            "type": "init",
-            "msg_id": 1,
-            "node_id":  "n3",
-            "node_ids": ["n1", "n2", "n3"]
+use maelstrom::{Body, EchoOk, Envelope, InitOk};
+use tokio::sync::mpsc;
+
+pub struct Node {
+    pub msg_id: u64,
+    pub node_id: String,
+    pub node_ids: Vec<String>,
+    pub sender: mpsc::Sender<Envelope>,
+    pub receiver: mpsc::Receiver<Envelope>,
+}
+
+impl Node {
+    async fn new(
+        sender: mpsc::Sender<Envelope>,
+        receiver: mpsc::Receiver<Envelope>,
+    ) -> tokio::task::JoinHandle<()> {
+        let mut node = Node {
+            msg_id: 0,
+            node_id: String::new(),
+            node_ids: Vec::new(),
+            receiver,
+            sender,
+        };
+        tokio::spawn(async move {
+            node.run().await;
+        })
+    }
+
+    async fn run(&mut self) {
+        while let Some(env) = self.receiver.recv().await {
+            match env.body {
+                Body::Init(init) => {
+                    self.msg_id += 1;
+                    self.node_id = init.node_id;
+                    self.node_ids = init.node_ids;
+                    self.sender
+                        .send(Envelope {
+                            src: self.node_id.clone(),
+                            dest: env.src,
+                            body: Body::InitOk(InitOk {
+                                msg_id: self.msg_id,
+                                in_reply_to: init.msg_id,
+                            }),
+                        })
+                        .await
+                        .unwrap();
+                }
+                Body::Echo(echo) => {
+                    self.msg_id += 1;
+
+                    self.sender
+                        .send(Envelope {
+                            src: self.node_id.clone(),
+                            dest: env.src,
+                            body: Body::EchoOk(EchoOk {
+                                msg_id: self.msg_id,
+                                in_reply_to: echo.msg_id,
+                                echo: echo.echo,
+                            }),
+                        })
+                        .await
+                        .unwrap();
+                }
+                _ => {
+                    println!("unknown message received");
+                }
+            }
         }
     }
-    "#;
-    let init_msg: InitMsg = serde_json::from_str(init_msg_str)?;
-    println!("{:?}", init_msg);
+}
 
-    let node = Node {
-        node_id: init_msg.body.node_id.clone(),
-        node_ids: init_msg.body.node_ids.clone(),
-    };
+#[tokio::main]
+async fn main() {
+    let (tx, rx) = mpsc::channel::<Envelope>(32);
+    let (tx2, mut rx2) = mpsc::channel::<Envelope>(32);
 
-    let init_ok = InitOk {
-        src: node.node_id.clone(),
-        dest: init_msg.src.clone(),
-        body: InitOkBody {
-            msg_type: String::from("init_ok"),
-            in_reply_to: init_msg.body.msg_id,
-        },
-    };
-    println!("{:?}", init_ok);
+    let _ = tokio::spawn(async move {
+        while let Some(env) = rx2.recv().await {
+            let reply = serde_json::to_string(&env).unwrap();
+            println!("{}", reply);
+        }
+    });
 
-    let data = r#"
-    {
-        "src": "c1",
-        "dest": "n3",
-        "body": {
-            "type": "echo",
-            "msg_id": 1,
-            "echo": "Please echo 35"
+    let _ = Node::new(tx2, rx).await;
+
+    let mut lines = BufReader::new(stdin()).lines();
+
+    while let Ok(Some(line)) = lines.next_line().await {
+        match serde_json::from_str::<Envelope>(&line) {
+            Ok(env) => match tx.send(env).await {
+                Ok(()) => {}
+                Err(error) => eprintln!("message failed to send: {}", error),
+            },
+            Err(error) => {
+                eprintln!("failed to parse incoming message: {}", error);
+                eprintln!("input line was: {}", line);
+                std::process::exit(1);
+            }
         }
     }
-    "#;
-    let echo_msg: EchoMsg = serde_json::from_str(data)?;
-    println!("{:?}", echo_msg);
-    let echo_reply = EchoOk {
-        src: node.node_id.clone(),
-        dest: echo_msg.src.clone(),
-        body: EchoOkBody {
-            msg_type: String::from("echo_ok"),
-            msg_id: echo_msg.body.msg_id,
-            in_reply_to: echo_msg.body.msg_id,
-            echo: echo_msg.body.echo,
-        },
-    };
-    println!("{:?}", echo_reply);
-    Ok(())
 }
