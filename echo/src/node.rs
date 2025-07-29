62bb1f2fec18c7b11015afdebd06d7ecef109911
charlieroth <mail@charlieroth.me>
2025-07-29 13:47:35 +0200

    Create module structure in `echo/`

---
+++ echo/src/node.rs
@@ -0,0 +1,35 @@
+use maelstrom::{
+    Message, MessageBody,
+    node::{MessageHandler, Node},
+};
+
+pub struct EchoNode;
+
+impl MessageHandler for EchoNode {
+    fn handle(&mut self, node: &mut Node, message: Message) -> Vec<Message> {
+        let mut out: Vec<Message> = Vec::new();
+        match message.body {
+            MessageBody::Init {
+                msg_id,
+                node_id,
+                node_ids,
+            } => {
+                node.handle_init(node_id, node_ids);
+                out.push(node.init_ok(message.src, msg_id));
+            }
+            MessageBody::Echo { msg_id, echo } => {
+                let response_msg_id = node.next_msg_id();
+                out.push(node.reply(
+                    message.src,
+                    MessageBody::EchoOk {
+                        msg_id: response_msg_id,
+                        in_reply_to: msg_id,
+                        echo,
+                    },
+                ));
+            }
+            _ => {}
+        }
+        out
+    }
+}
